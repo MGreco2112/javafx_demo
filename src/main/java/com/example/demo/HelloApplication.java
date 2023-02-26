fba24c4ded47de8800393984f561762c9c015bab
Matthew Greco <mgreco2112@tutanota.com>
2023-02-26 18:39:01 -0500

    moved resolution variables to scene constructor as opposed to stage fields

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -22,13 +22,12 @@ public class HelloApplication extends Application { //extend parent class Applic
     public void start(Stage stage) throws Exception { //start method called by launch method in main, takes Stage as parameter
         //stage generated and passed in from launch method via launchimplementation call
         Group root = new Group(); //instantiates new Group object, which is a node for the scene
-        Scene scene = new Scene(root, Color.BLACK); //instantiates new Scene for the stage, node is passed into scene constructor
+        Scene scene = new Scene(root, 640, 480, Color.BLACK); //instantiates new Scene for the stage, node is passed into scene constructor
+        //width and height variables passed into scene constructor
         //color argument for constructor can change the background of the scene (default state is white)
         stage.setTitle("Area 401"); //sets title for the window when stage is shown
         Image icon = new Image(new FileInputStream("src/logo.jpg")); //instantiates Image object from FileInputStream local path of image
         stage.getIcons().add(icon); //adds image as icon to Stage icons
-        stage.setWidth(640); //set width in pixels for stage
-        stage.setHeight(480); //set height in pixels for stage
         stage.setResizable(false); //set resizable ability by user (default is true)
         stage.setScene(scene); //scene is set onto the stage
 
